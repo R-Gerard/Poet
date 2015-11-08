02b58d4e46f872c45ea79621f279a3c63a6c9642
Rusty Gerard <rusty.gerard@gmail.com>
2015-11-07 17:40:14 -0800

    Add the public key for RVM

--- install.sh
+++ install.sh
@@ -15,6 +15,8 @@ iptables -F
 # Install RVM, Ruby, Rubygems, Bundler, and required gems
 yum -y remove ruby
 
+gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
 curl -L https://get.rvm.io | bash -s stable --ruby
 source /usr/local/rvm/scripts/rvm
 source /etc/profile.d/rvm.sh
